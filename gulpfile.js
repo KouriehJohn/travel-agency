3fdd591f0f8bc2ca809ec49c49924dcf13a989fd
John <hkourieh@icloud.com>
2017-06-06 23:54:15 -0700

    added browserSync to html and css

--- gulpfile.js
+++ gulpfile.js
@@ -26,19 +26,24 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
 
   browserSync.init({
+    notify: false, //if true, or not stated. Shows the updated injection.
     server: {
       baseDir: "app"
     }
   });
 
   watch('./app/index.html', function() {
-    gulp.start('html');
-    // This will WATCH for any changes made to the HTML file.
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
     // This will WATCH for any changes made to the CSS file.
   })
 
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
